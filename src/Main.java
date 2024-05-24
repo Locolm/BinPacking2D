04e950ce72c9ef8a141f08c88823d9be637a8c33
Locolm <clement.hl34@gmail.com>
2024-05-24 23:33:57 +0200

    refacto end

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Lecteur.readFileBP2("Ressources/binPacking2d-12.bp2d");
+        Lecteur.readFileBP2("Ressources/binPacking2d-04.bp2d");
         BinPackingSolver binPackingSolver = new BinPackingSolver(Lecteur.width, Lecteur.height, Lecteur.items);
         binPackingSolver.init();
 
